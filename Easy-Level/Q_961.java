9a3cd72d6e1c1dbef99ba75076b6aaec3fdba1c2
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-04-08 19:55:25 +0530

    N-Repeated Element in Size 2N Array

---
+++ Easy-Level/Q_961.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class Q_961 {
+  
+    public int repeatedNTimes(int[] nums) {
+      // method 1
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+             if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+
+        return -1;
+
+        
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int num : nums){
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+        // for(int cnt : map.keySet()){
+        //     if(map.get(cnt) > 1){
+        //         return cnt;
+        //     }
+        // }
+       
+        // return -1;
+    }
+
+}
