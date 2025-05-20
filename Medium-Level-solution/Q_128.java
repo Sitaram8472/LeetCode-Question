9bc91d585c40ea125fda7cbd9bbee1ee46a2e9e6
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-05-20 20:57:26 +0530

    Longest Consecutive Sequence

--- Medium-Level-solution/Q_128.java
+++ Medium-Level-solution/Q_128.java
@@ -1,3 +1,24 @@
 public class Q_128 {
-  
+  public int longestConsecutive(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+    HashSet<Integer> set = new HashSet<>();
+    for (int num : nums) {
+      set.add(num);
+    }
+    int max = 1;
+    for (int num : set) {
+      if (!set.contains(num - 1)) {
+        int currcnt = 1;
+
+        while (set.contains(num + currcnt)) {
+          currcnt++;
+        }
+        max = Math.max(currcnt, max);
+      }
+
+    }
+    return max;
+  }
 }
