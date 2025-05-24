5e5c791070cf642e2f01ef8ab013479bd610e690
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-05-24 19:18:30 +0530

    Find Words Containing Character

---
+++ Easy-Level-solution/Q_2942.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        int n = words.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(words[i].indexOf(x) != -1){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
