d419520046445e1a0f6f276fcd11d8b11d753ac2
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-05-26 23:30:46 +0530

    Add Digits

---
+++ Easy-Level-solution/Q_258.java
@@ -0,0 +1,25 @@
+public class Q_258 {
+
+  public int addDigits(int num) {
+    return res(num);
+
+  }
+
+  public static int res(int n) {
+    if (n < 10) {
+      return n;
+    }
+
+    while (n > 9) {
+      int ans = 0;
+      while (n > 0) {
+        int rem = n % 10;
+        ans += rem;
+        n /= 10;
+      }
+      n = ans;
+
+    }
+    return n;
+  }
+}
