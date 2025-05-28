5a22d1959919be4b542ae25246b9f2a84cda53b1
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-05-28 20:07:38 +0530

    Left and Right Sum Differences

--- Easy-Level-solution/Q_2575.java
+++ Easy-Level-solution/Q_2575.java
@@ -1,3 +1,22 @@
 public class Q_2575 {
-  
+  public int[] leftRightDifference(int[] nums) {
+    int n = nums.length;
+
+    int[] ans = new int[n];
+
+    int rightsum = 0;
+    for (int i = 0; i < n; i++) {
+      rightsum += nums[i];
+    }
+
+    int leftsum = 0;
+    for (int i = 0; i < n; i++) {
+      rightsum -= nums[i];
+      ans[i] = Math.abs(leftsum - rightsum);
+      leftsum += nums[i];
+    }
+
+    return ans;
+
+  }
 }
