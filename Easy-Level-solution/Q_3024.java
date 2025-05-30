6a6a9bf91311c2f59a2b1c88cde4df08b50a6d59
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-05-30 23:29:01 +0530

    Type of Triangle

--- Easy-Level-solution/Q_3024.java
+++ Easy-Level-solution/Q_3024.java
@@ -1,3 +1,22 @@
 public class Q_3024 {
-  
+  public String triangleType(int[] nums) {
+
+    int sum1 = nums[0] + nums[1];
+    int sum2 = nums[0] + nums[2];
+    int sum3 = nums[1] + nums[2];
+
+    if (sum1 > nums[2] && sum2 > nums[1] && sum3 > nums[0]) {
+
+      if (nums[0] == nums[1] && nums[1] == nums[2]) {
+        return "equilateral";
+      } else if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]) {
+        return "isosceles";
+      } else {
+        return "scalene";
+      }
+
+    }
+
+    return "none";
+  }
 }
