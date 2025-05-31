526aed8002fa49dbc0296c400dbbd6c62b8b8a88
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-05-31 23:35:52 +0530

    Count Operations to Obtain Zero

--- Easy-Level-solution/Q_2169.java
+++ Easy-Level-solution/Q_2169.java
@@ -1,3 +1,21 @@
 public class Q_2169 {
-  
+  public int countOperations(int num1, int num2) {
+    if (num1 == 0 || num2 == 0) {
+      return 0;
+    }
+
+    int oper = 0;
+    while (num1 != 0 && num2 != 0) {
+      if (num1 >= num2) {
+        num1 -= num2;
+        oper++;
+      } else if (num2 >= num1) {
+        num2 -= num1;
+        oper++;
+      }
+    }
+
+    return oper;
+
+  }
 }
