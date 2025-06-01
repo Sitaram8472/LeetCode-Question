e6e79ed06d2ca790e575a69cd9001afa166edd20
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-01 23:49:14 +0530

    Ransom Note

--- Easy-Level-solution/Q_383.java
+++ Easy-Level-solution/Q_383.java
@@ -1,3 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q_383 {
-  
+  public boolean canConstruct(String ransomNote, String magazine) {
+
+    Map<Character, Integer> freq1 = new HashMap<>();
+    Map<Character, Integer> freq2 = new HashMap<>();
+
+    for (char ch : ransomNote.toCharArray()) {
+      freq1.put(ch, freq1.getOrDefault(ch, 0) + 1);
+    }
+
+    for (char ch : magazine.toCharArray()) {
+      freq2.put(ch, freq2.getOrDefault(ch, 0) + 1);
+    }
+
+    for (char n : freq1.keySet()) {
+      if (freq2.getOrDefault(n, 0) < freq1.get(n)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
