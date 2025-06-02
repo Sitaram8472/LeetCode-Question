33f9ea17e8696dd9abbc359011a5115c4d6008e7
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-02 20:55:43 +0530

    Count Distinct Numbers on Board

--- Easy-Level-solution/Q_2549.java
+++ Easy-Level-solution/Q_2549.java
@@ -1,3 +1,13 @@
 public class Q_2549 {
-  
+  public int distinctIntegers(int n) {
+    int cnt = 1;
+
+    for (int i = n; i > 2; i--) {
+      if (n % (n - 1) == 1) {
+        cnt++;
+      }
+    }
+
+    return cnt;
+  }
 }
