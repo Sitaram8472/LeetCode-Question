56d8b7ee3662f402c9a3111ba58a325614017e66
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-05 23:25:17 +0530

    Minimum Operations to Make Array Equal

--- Medium-Level-solution/Q_1551.java
+++ Medium-Level-solution/Q_1551.java
@@ -2,7 +2,7 @@ public class Q_1551 {
   public int minOperations(int n) {
     int oper = 0;
 
-    for (int i = 1; i < n; i += 2) {
+    for (int i = 1; i < n; i += 2) {  
       oper += (n - i);
     }
 
