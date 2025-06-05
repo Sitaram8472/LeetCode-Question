2f679bd8afc0cbcf7fb572e6b84857c4f27cff87
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-05 23:22:52 +0530

    Count the Number of Vowel Strings in Range

--- Easy-Level-solution/Q_2586.java
+++ Easy-Level-solution/Q_2586.java
@@ -1,3 +1,16 @@
 public class Q_2586 {
-  
+  public int vowelStrings(String[] words, int left, int right) {
+    int n = words.length;
+    String vowel = "aeiou";
+    int count = 0;
+    for (int i = left; i <= right; i++) {
+      String str = words[i];
+      char start = str.charAt(0);
+      char end = str.charAt(str.length() - 1);
+      if (vowel.indexOf(start) != -1 && vowel.indexOf(end) != -1) {
+        count++;
+      }
+    }
+    return count;
+  }
 }
