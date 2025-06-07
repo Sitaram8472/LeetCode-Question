d55429353c8c67075546b29d6d9f88387752ec2d
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-07 12:35:22 +0530

    Minimum Cost of Buying Candies With Discount

--- Easy-Level-solution/Q_2144.java
+++ Easy-Level-solution/Q_2144.java
@@ -1,3 +1,17 @@
+import java.util.Arrays;
+
 public class Q_2144 {
-  
+  public int minimumCost(int[] cost) {
+    Arrays.sort(cost);
+
+    int mincost = 0;
+    int cnt = 0;
+    for (int i = cost.length - 1; i >= 0; i--) {
+      if ((cost.length - i) % 3 != 0) {
+        mincost += cost[i];
+      }
+    }
+
+    return mincost;
+  }
 }
