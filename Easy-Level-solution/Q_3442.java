9fca7fff5950c775908205807901bc28e9613eb4
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-10 21:10:19 +0530

    Maximum Difference Between Even and Odd Frequency I

--- Easy-Level-solution/Q_3442.java
+++ Easy-Level-solution/Q_3442.java
@@ -1,3 +1,26 @@
 public class Q_3442 {
-  
+  public int maxDifference(String s) {
+    int n = s.length();
+
+    int arr[] = new int[26];
+    for (int i = 0; i < n; i++) {
+      char ch = s.charAt(i);
+      arr[ch - 'a']++;
+    }
+    int oddmax = 0;
+    int evenmax = Integer.MAX_VALUE;
+    for (int i = 0; i < 26; i++) {
+      if (arr[i] % 2 != 0) {
+        oddmax = Math.max(oddmax, arr[i]);
+      } else {
+        if (arr[i] > 0) {
+          evenmax = Math.min(evenmax, arr[i]);
+        }
+
+      }
+    }
+
+    return oddmax - evenmax;
+
+  }
 }
