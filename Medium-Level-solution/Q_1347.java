46f1408f4ae65ae52428c7058b526001b94a7004
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-11 21:22:00 +0530

    Minimum Number of Steps to Make Two Strings Anagram

--- Medium-Level-solution/Q_1347.java
+++ Medium-Level-solution/Q_1347.java
@@ -1,3 +1,22 @@
 public class Q_1347 {
-  
+  public int minSteps(String s, String t) {
+    int n = s.length();
+    int n2 = t.length();
+
+    int[] freq = new int[26];
+    for (char ch : s.toCharArray()) {
+      int ch2 = ch - 'a';
+      freq[ch2]++;
+    }
+
+    for (char ch : t.toCharArray()) {
+      int ch2 = ch - 'a';
+      freq[ch2]--;
+    }
+    int ans = 0;
+    for (int i = 0; i < 26; i++) {
+      ans += Math.max(0, freq[i]);
+    }
+    return ans;
+  }
 }
