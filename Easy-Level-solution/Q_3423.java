138c90ec61402c9bb4cfddaa82c0853ef82f55ed
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-12 23:35:28 +0530

    Maximum Difference Between Adjacent Elements in a Circular Array

--- Easy-Level-solution/Q_3423.java
+++ Easy-Level-solution/Q_3423.java
@@ -1,3 +1,13 @@
 public class Q_3423 {
-  
+  public int maxAdjacentDistance(int[] nums) {
+    int n = nums.length;
+    int last = Math.abs(nums[n - 1] - nums[0]);
+    int max = Integer.MIN_VALUE;
+
+    for (int i = 0; i < n - 1; i++) {
+      max = Math.max(Math.abs(nums[i] - nums[i + 1]), max);
+    }
+    max = Math.max(max, last);
+    return max;
+  }
 }
