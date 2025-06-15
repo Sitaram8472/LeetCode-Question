18fe0f27eab7a07555043f952587fc9023625427
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-15 22:58:46 +0530

    Minimum Impossible OR

--- Medium-Level-solution/Q_2568.java
+++ Medium-Level-solution/Q_2568.java
@@ -1,3 +1,15 @@
+import java.util.Arrays;
+
 public class Q_2568 {
-  
+  public int minImpossibleOR(int[] nums) {
+    Arrays.sort(nums);
+    int minxor = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] > minxor + 1) {
+        return minxor + 1;
+      }
+      minxor = minxor | nums[i];
+    }
+    return minxor + 1;
+  }
 }
