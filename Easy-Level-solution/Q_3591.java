7b8c70352ab98e51d0315c07a91e7b92086263c5
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-22 20:10:58 +0530

    Check if Any Element Has Prime Frequency

--- Easy-Level-solution/Q_3591.java
+++ Easy-Level-solution/Q_3591.java
@@ -1,3 +1,34 @@
 public class Q_3591 {
-  
+   public boolean checkPrimeFrequency(int[] nums) {
+
+        int n = nums.length;
+        int[] arr = new int[101];
+
+        for (int i = 0; i < n; i++) {
+            arr[nums[i]]++;
+        }
+
+        for (int i = 0; i < 101; i++) {
+            if(arr[i] <= 1){
+                continue;
+            }
+            if (arr[i] > 1) {
+                if (isprime(arr[i])) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+
+    }
+
+    private boolean isprime(int num) {
+        for (int j = 2; j<= (int)Math.sqrt(num); j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
