eef966fd5bcc5746e38104208cbe9f0c0600fcd2
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-06-26 21:54:48 +0530

    First Letter to Appear Twice

--- Easy-Level-solution/Q_2351.java
+++ Easy-Level-solution/Q_2351.java
@@ -1,3 +1,30 @@
+import java.util.HashSet;
+
 public class Q_2351 {
-  
+  public char repeatedCharacter(String s) {
+    // method 1
+
+    // char ch[] = new char[26];
+    // for(int i=0; i<s.length(); i++){
+    // ch[s.charAt(i)-'a']++;
+    // for(int j=0; j<26; j++){
+    // if(ch[j] == 2){
+    // return s.charAt(i);
+    // }
+    // }
+    // }
+    // return 'm';
+
+    // method 2
+
+    HashSet<Character> set = new HashSet<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (set.contains(s.charAt(i))) {
+        return s.charAt(i);
+      } else {
+        set.add(s.charAt(i));
+      }
+    }
+    return 'm';
+  }
 }
