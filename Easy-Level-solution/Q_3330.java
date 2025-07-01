ecf57dc4f77ba94e4c9c898862582a09d14ca66f
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-01 22:35:34 +0530

    Find the Original Typed String I

--- Easy-Level-solution/Q_3330.java
+++ Easy-Level-solution/Q_3330.java
@@ -1,3 +1,11 @@
 public class Q_3330 {
-  
+  public int possibleStringCount(String word) {
+    int cnt = 1;
+    for (int i = 1; i < word.length(); i++) {
+      if (word.charAt(i) == word.charAt(i - 1)) {
+        cnt++;
+      }
+    }
+    return cnt;
+  }
 }
