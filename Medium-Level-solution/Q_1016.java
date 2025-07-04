5e7a54f02130bde0e366a87a4c0031240b65eaf2
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-04 07:58:31 +0530

    Binary String With Substrings Representing 1 To N

--- Medium-Level-solution/Q_1016.java
+++ Medium-Level-solution/Q_1016.java
@@ -1,3 +1,16 @@
 public class Q_1016 {
-  
+  public boolean queryString(String s, int n) {
+
+    for (int i = 1; i <= n; i++) {
+      String binary = Integer.toBinaryString(i);
+
+      if (!s.contains(binary)) {
+        return false;
+      }
+
+    }
+
+    return true;
+
+  }
 }
