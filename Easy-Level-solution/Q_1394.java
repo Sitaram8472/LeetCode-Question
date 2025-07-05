d57f213d11c87aac6ee2b2bd192c36ccf29a95a6
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-05 16:03:06 +0530

    Find Lucky Integer in an Array

--- Easy-Level-solution/Q_1394.java
+++ Easy-Level-solution/Q_1394.java
@@ -1,3 +1,22 @@
+import java.util.HashMap;
+
 public class Q_1394 {
-  
+  public int findLucky(int[] arr) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int num : arr) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+
+    }
+    int lucky = -1;
+
+    for (int num : map.keySet()) {
+      if (num == map.get(num)) {
+        if (num > lucky) {
+          lucky = num;
+        }
+      }
+    }
+
+    return lucky;
+  }
 }
