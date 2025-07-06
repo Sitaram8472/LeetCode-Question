57ab10842b5475cf6e8fe187dd9622fc822ed3dc
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-06 23:34:29 +0530

    Find Smallest Letter Greater Than Target

--- Easy-Level-solution/Q_744.java
+++ Easy-Level-solution/Q_744.java
@@ -1,4 +1,14 @@
 public class Q_744 {
 
-  
+  public char nextGreatestLetter(char[] letters, char target) {
+    int n = letters.length;
+
+    for (int i = 0; i < n; i++) {
+      if (letters[i] > target) {
+        return letters[i];
+      }
+    }
+    return letters[0];
+
+  }
 }
