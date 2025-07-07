9c56b5a0309fb3e6564fb40c0585d5b0aae9cf10
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-07 21:52:06 +0530

    Find the Divisibility Array of a String

--- Medium-Level-solution/Q_2575.java
+++ Medium-Level-solution/Q_2575.java
@@ -1,3 +1,16 @@
 public class Q_2575 {
-  
+  public int[] divisibilityArray(String word, int m) {
+    int n = word.length();
+    int[] arr = new int[n];
+    long rem = 0;
+
+    for (int i = 0; i < word.length(); i++) {
+      char ch = word.charAt(i);
+      int currdigit = ch - '0';
+      rem = ((rem * 10) + currdigit) % m;
+      arr[i] = (rem == 0) ? 1 : 0;
+    }
+    return arr;
+
+  }
 }
