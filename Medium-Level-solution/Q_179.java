6302b92b3310a4136579d0cdf1b3f91738e8a192
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-08 09:25:54 +0530

    Largest Number

--- Medium-Level-solution/Q_179.java
+++ Medium-Level-solution/Q_179.java
@@ -1,3 +1,24 @@
 public class Q_179 {
-  
+  public String largestNumber(int[] nums) {
+        String[] strnum = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strnum[i] = Integer.toString(nums[i]);
+        }
+
+
+        Arrays.sort(strnum, (a, b) -> (ab+).compareTo(a+b));
+
+        if(strnum[0].equals("0")){
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(String str : strnum){
+            sb.append(str);
+        }
+
+        return sb.toString();
+
+    }
 }
