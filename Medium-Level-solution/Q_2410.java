465dd8979432ecb7f9c57e7930309e37a711a04c
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-09 21:31:22 +0530

    Maximum Matching of Players With Trainers

--- Medium-Level-solution/Q_2410.java
+++ Medium-Level-solution/Q_2410.java
@@ -1,4 +1,26 @@
+import java.util.Arrays;
+
 public class Q_2410 {
-  
-  
+  public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+    int p = players.length;
+    int t = trainers.length;
+
+    Arrays.sort(players);
+    Arrays.sort(trainers);
+
+    int cnt = 0;
+
+    int i = 0;
+    int j = 0;
+    while (i < p && j < t) {
+      if (players[i] <= trainers[j]) {
+        i++;
+        cnt++;
+      }
+      j++;
+    }
+    return cnt;
+
+  }
+
 }
