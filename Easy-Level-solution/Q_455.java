e8de2c98a2673048088a98598f83315f9c57f752
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-10 21:21:32 +0530

    Assign Cookies

--- Easy-Level-solution/Q_455.java
+++ Easy-Level-solution/Q_455.java
@@ -1,3 +1,22 @@
+import java.util.Arrays;
+
 public class Q_455 {
-  
+  public int findContentChildren(int[] g, int[] s) {
+    int cnt = 0;
+
+    Arrays.sort(g);
+    Arrays.sort(s);
+
+    int i = 0;
+    int j = 0;
+    while (i < g.length && j < s.length) {
+      if (g[i] <= s[j]) {
+        i++;
+        cnt++;
+      }
+      j++;
+    }
+    return cnt;
+
+  }
 }
