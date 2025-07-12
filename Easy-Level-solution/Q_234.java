1e60503cc331c9b2054aa689222dd3c8ad3a5790
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-12 21:39:47 +0530

    Palindrome Linked List

--- Easy-Level-solution/Q_234.java
+++ Easy-Level-solution/Q_234.java
@@ -1,3 +1,43 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
 public class Q_234 {
-  
+  public boolean isPalindrome(ListNode head) {
+    ListNode mid = foundmid(head);
+    // reverse
+
+    ListNode prev = null;
+    ListNode curr = mid;
+    ListNode next;
+    while (curr != null) {
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+
+    ListNode right = prev; // right half head
+    ListNode left = head; // left first head
+
+    while (right != null) {
+      if (left.val != right.val) {
+        return false;
+      }
+      left = left.next;
+      right = right.next;
+    }
+
+    return true;
+
+  }
+
+  public static ListNode foundmid(ListNode data) {
+    ListNode slow = data;
+    ListNode fast = data;
+
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
 }
